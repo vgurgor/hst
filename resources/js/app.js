043e14d034ad24fe23b5bcd72d1bbce511eb8172
Volkan GÜRGÖR <volkangurgor@gmail.com>
2023-06-07 08:02:25 +0300

    dil özelliği eklendi,
    menü düzenlendi,
    
    dark light switch eklendi

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,22 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+if (localStorage.theme === 'dark') {
+    document.documentElement.classList.add('dark');
+    document.getElementById("theme-toggle").checked = true;
+} else {
+    document.documentElement.classList.remove('dark');
+    document.getElementById("theme-toggle").checked = false;
+}
+document.getElementById('theme-toggle').addEventListener('change', function () {
+    // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+    if (localStorage.theme === 'dark') {
+        document.documentElement.classList.remove('dark');
+        localStorage.theme = 'light';
+    } else {
+        document.documentElement.classList.add('dark');
+        localStorage.theme = 'dark';
+    }
+});
+
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -11,9 +11,63 @@
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex sm:flex sm:items-center ">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
+                    </x-nav-link>
+                    <x-dropdown align="left">
+                        <x-slot name="trigger">
+                            <a href="#" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                                {{ __('Okul Yönetimi') }}
+                            </a>
+                        </x-slot>
+
+                        <x-slot name="content" >
+                            <x-dropdown-link :href="route('dashboard')" :active="request()->routeIs('okul.index')">
+                                {{ __('Kampüs') }}
+                            </x-dropdown-link>
+                            <x-dropdown-link :href="route('dashboard')" :active="request()->routeIs('okul.create')">
+                                {{ __('Şube') }}
+                            </x-dropdown-link>
+                            <x-dropdown-link :href="route('dashboard')" :active="request()->routeIs('okul.create')">
+                                {{ __('Düzey') }}
+                            </x-dropdown-link>
+                            <x-dropdown-link :href="route('dashboard')" :active="request()->routeIs('okul.create')">
+                                {{ __('Sınıf') }}
+                            </x-dropdown-link>
+                            <x-dropdown-link :href="route('dashboard')" :active="request()->routeIs('okul.create')">
+                                {{ __('Branş') }}
+                            </x-dropdown-link>
+                        </x-slot>
+                    </x-dropdown>
+                    <x-dropdown align="left">
+                        <x-slot name="trigger">
+                            <a href="#" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                                {{ __('Öğretmen Yönetimi') }}
+                            </a>
+                        </x-slot>
+
+                        <x-slot name="content" >
+                            <x-dropdown-link :href="route('dashboard')" :active="request()->routeIs('okul.index')">
+                                {{ __('Öğretmen') }}
+                            </x-dropdown-link>
+                        </x-slot>
+                    </x-dropdown>
+                    <x-dropdown align="left">
+                        <x-slot name="trigger">
+                            <a href="#" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                                {{ __('Ders Yönetimi') }}
+                            </a>
+                        </x-slot>
+
+                        <x-slot name="content" >
+                            <x-dropdown-link :href="route('dashboard')" :active="request()->routeIs('okul.index')">
+                                {{ __('Ağırlık Dağıtımı') }}
+                            </x-dropdown-link>
+                        </x-slot>
+                    </x-dropdown>
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs(' Çizgelge Oluşturucu')">
+                        {{ __(' Çizgelge Oluşturucu') }}
                     </x-nav-link>
                 </div>
             </div>
