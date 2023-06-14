e7797748091542341bdc21a86858c0f5f7635c57
Volkan GÜRGÖR <volkangurgor@gmail.com>
2023-06-14 11:17:05 +0300

    test15

--- app/Http/Controllers/WEB/TimetableCreatorController.php
+++ app/Http/Controllers/WEB/TimetableCreatorController.php
@@ -466,7 +466,7 @@ class TimetableCreatorController extends Controller
         $json = json_decode($content);
         $array = (array) $json;
 
-        dd($array[0]["classroom_id"]);
+        dd($array[0]->classroom_id);
 
         $data = '<div class="w-full text-center"> <h1><span class="bi bi-check-circle-fill text-green-600" style="font-size:100px"></span></h1><br><h2 class="text-xl">'.__("Çizelge oluşturudu").'</h2> </div>';
         echo json_encode(array("status"=>"success", "data"=>$data));
