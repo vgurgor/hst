655d45d42ab08f8c0916d5e22e5d916cc2d9f143
Volkan GÜRGÖR <volkangurgor@gmail.com>
2023-06-14 15:13:04 +0300

    some fixes 6

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,38 +10,34 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <h1 style="font-weight: bold;font-size:32px" class="mb-4 text-red-600">{{__("Çizelgesi Oluşturulmuş Sınıflar")}}</h1>
-                    <div class="table w-full border-collapse border border-slate-500">
-                        <div class="table-header-group font-bold ">
-                            <div class="table-row">
-                                <div class="table-cell text-left border border-slate-600 p-2">{{__("Kampüs")}}</div>
-                                <div class="table-cell text-left border border-slate-600 p-2">{{__("Şube")}}</div>
-                                <div class="table-cell text-left border border-slate-600 p-2">{{__("Düzey")}}</div>
-                                <div class="table-cell text-left border border-slate-600 p-2">{{__("Sınıf")}}</div>
-                                <div class="table-cell text-left border border-slate-600 p-2 text-center">{{__("Görüntüle")}}</div>
-                            </div>
-                        </div>
-                        @if(empty($timetables))
-                            <div class="table-row">
-                                <div colSpan={6}  class="table-cell border border-slate-600 p-2" colspan="5">
-                                    <a href="{{ route('timetablecreator.wizard') }}" target="_blank">{{__('Çizelge Oluştur')}}</a>
+                    @if(empty($timetables))
+                        <div class="text-center"><a href="{{ route('timetablecreator.wizard' ) }}" target="_blank">{{__('Çizelge Oluştur')}}</a></div>
+                    @else
+                        <div class="table w-full border-collapse border border-slate-500">
+                            <div class="table-header-group font-bold ">
+                                <div class="table-row">
+                                    <div class="table-cell text-left border border-slate-600 p-2">{{__("Kampüs")}}</div>
+                                    <div class="table-cell text-left border border-slate-600 p-2">{{__("Şube")}}</div>
+                                    <div class="table-cell text-left border border-slate-600 p-2">{{__("Düzey")}}</div>
+                                    <div class="table-cell text-left border border-slate-600 p-2">{{__("Sınıf")}}</div>
+                                    <div class="table-cell text-left border border-slate-600 p-2 text-center">{{__("Görüntüle")}}</div>
                                 </div>
                             </div>
-                        @else
-                        <div class="table-row-group">
-                            @foreach ($timetables as $timetable)
-                            <div class="table-row">
-                                <div class="table-cell border border-slate-600 p-2">{{$timetable["campus_name"]}}</div>
-                                <div class="table-cell border border-slate-600 p-2">{{$timetable["branch_name"]}}</div>
-                                <div class="table-cell border border-slate-600 p-2">{{$timetable["grade_name"]}}</div>
-                                <div class="table-cell border border-slate-600 p-2">{{$timetable["classroom_name"]}}</div>
-                                <div class="table-cell border border-slate-600 p-2 text-center">
-                                    <a href="{{ route('timetable.class', $timetable["classroom_id"] ) }}" target="_blank"><span class="bi bi-eye-fill"></span></a>
+                            <div class="table-row-group">
+                                @foreach ($timetables as $timetable)
+                                <div class="table-row">
+                                    <div class="table-cell border border-slate-600 p-2">{{$timetable["campus_name"]}}</div>
+                                    <div class="table-cell border border-slate-600 p-2">{{$timetable["branch_name"]}}</div>
+                                    <div class="table-cell border border-slate-600 p-2">{{$timetable["grade_name"]}}</div>
+                                    <div class="table-cell border border-slate-600 p-2">{{$timetable["classroom_name"]}}</div>
+                                    <div class="table-cell border border-slate-600 p-2 text-center">
+                                        <a href="{{ route('timetable.class', $timetable["classroom_id"] ) }}" target="_blank"><span class="bi bi-eye-fill"></span></a>
+                                    </div>
                                 </div>
+                                @endforeach
                             </div>
-                            @endforeach
                         </div>
-                        @endif
-                    </div>
+                    @endif
                 </div>
             </div>
         </div>
