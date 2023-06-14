d01d903301b6cf650b245b3da688465986377706
Volkan GÜRGÖR <volkangurgor@gmail.com>
2023-06-14 07:14:17 +0300

    add vh

--- resources/views/pages/teacher/add.blade.php
+++ resources/views/pages/teacher/add.blade.php
@@ -12,7 +12,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100" style="min-height: 50vh;">
+                <div class="p-6 text-gray-900 dark:text-gray-100" style="min-height: 60vh;">
                     <form action="{{ route('teacher.store') }}" method="POST" autocomplete="off" class="mt-4" id="teacherForm">
 
                         <div class="flex">
